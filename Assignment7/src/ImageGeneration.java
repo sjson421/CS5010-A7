c399974190352b0038c3f00b11cf7d28f253d716
Shubham Singal <shubhamrsingal@gmail.com>
2019-11-07 17:47:06 -0500

    Vertical Rainbow

--- Assignment7/src/ImageGeneration.java
+++ Assignment7/src/ImageGeneration.java
@@ -92,7 +92,72 @@ public class ImageGeneration extends AbstractImageHandling {
   }
 
   private BufferedImage vertRainbowStripes(int width, int height) {
-    return null;
+    BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    int red;
+    int green;
+    int blue;
+    int pixel;
+
+    for (int i = 0; i < height; i++) { //rows
+      for (int j = 0; j < width; j++)  //columns
+
+        if (j < width / 7) { //color it red
+          red = 255;
+          green = 0;
+          blue = 0;
+          pixel = (red << 16) | (green << 8) | blue;
+          img.setRGB(j, i, pixel);
+
+        } else if (j < 2 * width / 7) {
+          //color it orange
+          red = 255;
+          green = 127;
+          blue = 0;
+          pixel = (red << 16) | (green << 8) | blue;
+          img.setRGB(j, i, pixel);
+        }
+        else if (j < 3 * width / 7) {
+          //color it yellow
+          red = 255;
+          green = 255;
+          blue = 0;
+          pixel = (red << 16) | (green << 8) | blue;
+          img.setRGB(j, i, pixel);
+        }
+        else if (j < 4 * width / 7) {
+          //color it green
+          red = 0;
+          green = 255;
+          blue = 0;
+          pixel = (red << 16) | (green << 8) | blue;
+
+          img.setRGB(j, i, pixel);
+        } else if (j < 5 * width / 7) {
+          //color it blue
+          red = 0;
+          green = 0;
+          blue = 255;
+          pixel = (red << 16) | (green << 8) | blue;
+          img.setRGB(j, i, pixel);
+        }
+        else if (j < 6 * width / 7) {
+          //color it indigo
+          red = 75;
+          green = 0;
+          blue = 130;
+          pixel = (red << 16) | (green << 8) | blue;
+          img.setRGB(j, i, pixel);
+        }
+        else{
+          //color it violet
+          red = 143;
+          green = 0;
+          blue = 255;
+          pixel = (red << 16) | (green << 8) | blue;
+          img.setRGB(j, i, pixel);
+        }
+    }
+    return img;
   }
 
   private BufferedImage checkerboard(int squareWidth, int squareHeight) {
