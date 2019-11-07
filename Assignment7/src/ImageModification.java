0dae8c3e5d28259fea5fa9cfe3a7954fa348165e
Jay Son <sjson421>
2019-11-07 18:52:44 -0500

    greyscale and sepia added

--- Assignment7/src/ImageModification.java
+++ Assignment7/src/ImageModification.java
@@ -45,11 +45,16 @@ public class ImageModification extends AbstractImageHandling {
   }
 
   private BufferedImage greyscale() {
-    return null;
+    BufferedImage returnImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    double[][] matrix = createGreyscaleColorMatrix();
+    return applyColorMatrix(returnImage, matrix);
   }
 
   private BufferedImage sepia() {
-    return null;
+
+    BufferedImage returnImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    double[][] matrix = createSepiaColorMatrix();
+    return applyColorMatrix(returnImage, matrix);
   }
 
   private BufferedImage applyFilter(BufferedImage returnImage, double[][] filter) {
@@ -67,21 +72,49 @@ public class ImageModification extends AbstractImageHandling {
               if (pixelUsedX >= 0 && pixelUsedY >= 0 && pixelUsedX < width
                       && pixelUsedY < height) {
                 int[] rgbProduct = getRGBForPixel(pixelUsedX, pixelUsedY);
-                filterSum[k] += rgbProduct[k] * filter[m + filterHalfLen][l + filterHalfLen];
+                filterSum[k] += rgbProduct[k] * filter[l + filterHalfLen][m + filterHalfLen];
               }
             }
           }
         }
-        int r = clamp(filterSum[0], 0, 255);
-        int g = clamp(filterSum[1], 0, 255);
-        int b = clamp(filterSum[2], 0, 255);
-        int rgb = (r << 16) | (g << 8) | b;
-        returnImage.setRGB(j, i, rgb);
+        setRGB(returnImage, filterSum, j, i);
       }
     }
     return returnImage;
   }
 
+  private BufferedImage applyColorMatrix(BufferedImage returnImage, double[][] matrix) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        int[] initialRGB = getRGBForPixel(j, i);
+        int[] appliedRGB = new int[RBG_LEN];
+        for (int l = 0; l < matrix.length; l++) {
+          for (int m = 0; m < matrix[l].length; m++) {
+            appliedRGB[l] += (int) Math.round(matrix[l][m] * initialRGB[m]);
+          }
+        }
+        setRGB(returnImage, appliedRGB, j, i);
+      }
+    }
+    return returnImage;
+  }
+
+  private void setRGB(BufferedImage returnImage, int[] colorMatrix, int j, int i) {
+    int r = clamp(colorMatrix[0], 0, 255);
+    int g = clamp(colorMatrix[1], 0, 255);
+    int b = clamp(colorMatrix[2], 0, 255);
+    int rgb = (r << 16) | (g << 8) | b;
+    returnImage.setRGB(j, i, rgb);
+  }
+
+  private int[] getRGBForPixel(int x, int y) {
+    int[] channels = new int[3];
+    int rgb = image.getRGB(x, y);
+    channels[0] = (rgb >> 16) & 0x000000FF;
+    channels[1] = (rgb >> 8) & 0x000000FF;
+    channels[2] = (rgb) & 0x000000FF;
+    return channels;
+  }
 
   private double[][] createBlurFilter() {
     double[][] filter = new double[3][3];
@@ -115,12 +148,33 @@ public class ImageModification extends AbstractImageHandling {
     return filter;
   }
 
-  private int[] getRGBForPixel(int x, int y) {
-    int[] channels = new int[3];
-    int rgb = image.getRGB(x, y);
-    channels[0] = (rgb >> 16) & 0x000000FF;
-    channels[1] = (rgb >> 8) & 0x000000FF;
-    channels[2] = (rgb) & 0x000000FF;
-    return channels;
+  private double[][] createGreyscaleColorMatrix() {
+    double[][] matrix = new double[3][3];
+    for (int y = 0; y < matrix.length; y++) {
+      for (int x = 0; x < matrix[y].length; x++) {
+        if (x == 0) {
+          matrix[y][x] = 0.2126;
+        } else if (x == 1) {
+          matrix[y][x] = 0.7152;
+        } else {
+          matrix[y][x] = 0.0722;
+        }
+      }
+    }
+    return matrix;
+  }
+
+  private double[][] createSepiaColorMatrix() {
+    double[][] matrix = new double[3][3];
+    matrix[0][0] = 0.393;
+    matrix[0][1] = 0.769;
+    matrix[0][2] = 0.189;
+    matrix[1][0] = 0.349;
+    matrix[1][1] = 0.686;
+    matrix[1][2] = 0.168;
+    matrix[2][0] = 0.272;
+    matrix[2][1] = 0.534;
+    matrix[2][2] = 0.131;
+    return matrix;
   }
 }
