a8337afc9aface1a666bc2a86f09b88d0ece5eee
Shubham Singal <shubhamrsingal@gmail.com>
2019-11-11 15:52:32 -0500

    Flags

--- Assignment7/src/Driver.java
+++ Assignment7/src/Driver.java
@@ -10,28 +10,30 @@ import javax.imageio.ImageIO;
 public class Driver {
   public static void main(String[] args) {
     try {
-//      BufferedImage image1 = ImageIO.read(new File("res/typical.jpg"));
-//      BufferedImage image2 = ImageIO.read(new File("res/landscape.jpg"));
-//      ImageHandling mod1 = new ImageModification(image1);
-//      ImageHandling mod2 = new ImageModification(image2);
+      BufferedImage image1 = ImageIO.read(new File("./res/typical.jpg"));
+      BufferedImage image2 = ImageIO.read(new File("./res/landscape.jpg"));
+      ImageHandling mod1 = new ImageModification(image1);
+      ImageHandling mod2 = new ImageModification(image2);
       ImageHandling gen = new ImageGeneration();
-//
-//      createImage(mod1, FilterType.BLUR, "res/typicalBlur.jpg");
-//      createImage(mod1, FilterType.SHARPEN, "res/typicalSharpen.jpg");
-//      createImage(mod1, FilterType.GREYSCALE, "res/typicalGreyScale.jpg");
-//      createImage(mod1, FilterType.SEPIA, "res/typicalSepia.jpg");
-//
-//      createImage(mod2, FilterType.BLUR, "res/landscapeBlur.jpg");
-//      createImage(mod2, FilterType.SHARPEN, "res/landscapeSharpen.jpg");
-//      createImage(mod2, FilterType.GREYSCALE, "res/landscapeGreyScale.jpg");
-//      createImage(mod2, FilterType.SEPIA, "res/landscapeSepia.jpg");
 
-      createImage(gen, GenerationType.HOR_RAINBOW_STRIPES, 1280, 720, "res/horRainbowStripes.jpg");
-      createImage(gen, GenerationType.VERT_RAINBOW_STRIPES, 1280, 720, "res/vertRainbowStripes.jpg");
-      createImage(gen, GenerationType.CHECKERBOARD, 100, "res/checkerboard.jpg");
-      createImage(gen, GenerationType.FLAG, FlagType.FRANCE, 1280, "res/flagFrance.jpg");
-      createImage(gen, GenerationType.FLAG, FlagType.GREECE, 1280, "res/flagGreece.jpg");
-      createImage(gen, GenerationType.FLAG, FlagType.SWITZERLAND, 1280, "res/flagSwitzerland.jpg");
+      createImage(mod1, FilterType.BLUR, "./res/typicalBlur.jpg");
+      createImage(mod1, FilterType.SHARPEN, "./res/typicalSharpen.jpg");
+      createImage(mod1, FilterType.GREYSCALE, "./res/typicalGreyScale.jpg");
+      createImage(mod1, FilterType.SEPIA, "./res/typicalSepia.jpg");
+
+      createImage(mod2, FilterType.BLUR, "./res/landscapeBlur.jpg");
+      createImage(mod2, FilterType.SHARPEN, "./res/landscapeSharpen.jpg");
+      createImage(mod2, FilterType.GREYSCALE, "./res/landscapeGreyScale.jpg");
+      createImage(mod2, FilterType.SEPIA, "./res/landscapeSepia.jpg");
+
+      File d = new File("./");
+      System.out.println(d.getAbsolutePath());
+      createImage(gen, GenerationType.HOR_RAINBOW_STRIPES, 1280, 720, "./res/horRainbowStripes.jpg");
+      createImage(gen, GenerationType.VERT_RAINBOW_STRIPES, 1280, 720, "./res/vertRainbowStripes.jpg");
+      createImage(gen, GenerationType.CHECKERBOARD, 100, "./res/checkerboard.jpg");
+      createImage(gen, GenerationType.FLAG, FlagType.FRANCE, 1280, "./res/flagFrance.jpg");
+      createImage(gen, GenerationType.FLAG, FlagType.GREECE, 1280, "./res/flagGreece.jpg");
+      createImage(gen, GenerationType.FLAG, FlagType.SWITZERLAND, 1280, "./res/flagSwitzerland.jpg");
 
     } catch (IOException e) {
       e.printStackTrace();
--- Assignment7/src/ImageGeneration.java
+++ Assignment7/src/ImageGeneration.java
@@ -154,11 +154,6 @@ public class ImageGeneration extends AbstractImageHandling {
 
   private BufferedImage checkerboard(int squareWidth) {
     BufferedImage img = new BufferedImage(squareWidth * 8, squareWidth * 8, BufferedImage.TYPE_INT_RGB);
-    int red;
-    int green;
-    int blue;
-    int pixel;
-
     for (int i = 0; i < squareWidth * 8; i++) {
       for (int j = 0; j < squareWidth * 8; j++) {
         if (i < squareWidth) {
@@ -239,12 +234,124 @@ public class ImageGeneration extends AbstractImageHandling {
 
   private BufferedImage generateFlag(int width, FlagType flagType) {
     if (flagType == FlagType.FRANCE) {
-
+      return getFranceFlag(width);
     } else if (flagType == FlagType.GREECE) {
-
+      return getGreeceFlag(width);
     } else if (flagType == FlagType.SWITZERLAND) {
-
+      return getSwissFlag(width);
     }
     return null;
+  }
+
+  private BufferedImage getFranceFlag(int width) {
+    int height = (2 * width) / 3;
+    BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    int red;
+    int green;
+    int blue;
+    int pixel;
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (j < width / 3) { //color it blue
+          red = 0;
+          green = 85;
+          blue = 164;
+        } else if (j < 2 * width / 3) { //color it white
+          red = 255;
+          green = 255;
+          blue = 255;
+        } else { //color it red
+          red = 239;
+          green = 65;
+          blue = 53;
+        }
+        pixel = (red << 16) | (green << 8) | blue;
+        img.setRGB(j, i, pixel);
+      }
+    }
+
+    return img;
+  }
+
+  private BufferedImage getSwissFlag(int width) {
+    BufferedImage img = new BufferedImage(width, width, BufferedImage.TYPE_INT_RGB);
+    int red;
+    int green;
+    int blue;
+
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < width; j++) {
+        if ((j >= 13 * width / 32 && j <= 19 * width / 32
+                && i >= 6 * width / 32 && i <= 26 * width / 32)
+                || (i >= 13.0 * width / 32.0 && i <= 19 * width / 32
+                && j >= 6 * width / 32 && j <= 26 * width / 32)) {
+          red = 255;
+          green = 255;
+          blue = 255;
+        } else {
+          red = 255;
+          green = 0;
+          blue = 0;
+        }
+        int pixel = (red << 16) | (green << 8) | blue;
+        img.setRGB(j, i, pixel);
+      }
+    }
+    return img;
+  }
+
+  private BufferedImage getGreeceFlag(int width) {
+    int height = (2 * width) / 3;
+    BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        if (j <= 10 * width / 27 && i <= 5 * height / 9) {
+          if ((j >= 4 * width / 27 && j <= 6 * width / 27)
+                  || (i >= 4 * width / 27 && i <= 6 * width / 27)) {
+            setGreeceWhite(img, j, i);
+          } else {
+            setGreeceBlue(img, j, i);
+          }
+        } else {
+          if (i <= height / 9) {
+            setGreeceBlue(img, j, i);
+          } else if (i <= 2 * height / 9) {
+            setGreeceWhite(img, j, i);
+          } else if (i <= 3 * height / 9) {
+            setGreeceBlue(img, j, i);
+          } else if (i <= 4 * height / 9) {
+            setGreeceWhite(img, j, i);
+          } else if (i <= 5 * height / 9) {
+            setGreeceBlue(img, j, i);
+          } else if (i <= 6 * height / 9) {
+            setGreeceWhite(img, j, i);
+          } else if (i <= 7 * height / 9) {
+            setGreeceBlue(img, j, i);
+          } else if (i <= 8 * height / 9) {
+            setGreeceWhite(img, j, i);
+          } else {
+            setGreeceBlue(img, j, i);
+          }
+        }
+      }
+    }
+    return img;
+  }
+
+  private void setGreeceWhite(BufferedImage img, int j, int i) {
+    int red = 255;
+    int green = 255;
+    int blue = 255;
+    int pixel = (red << 16) | (green << 8) | blue;
+    img.setRGB(j, i, pixel);
+  }
+
+  private void setGreeceBlue(BufferedImage img, int j, int i) {
+    int red = 0;
+    int green = 91;
+    int blue = 174;
+    int pixel = (red << 16) | (green << 8) | blue;
+    img.setRGB(j, i, pixel);
   }
 }
